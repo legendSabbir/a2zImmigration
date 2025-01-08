41f86b29c16a898f5344fe691330aff282b665bc
legendSabbir <sabbirhossain.trash@gmail.com>
2025-01-08 18:53:57 +0600

    welcome section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
       "use strict";
       $("#camera-slide").camera({
         height: '50%',
-        minHeight: window.innerHeight + "px",
+        minHeight: Math.min(window.innerHeight, 900) + "px",
 	      thumbnails: false,
         hover: true,
         fx: 'random',
@@ -40,7 +40,7 @@
 <main>
   <section class="mt-16 lg:mt-0">
     
-    <div class="camera_wrap overflow-hidden relative" id="camera-slide">
+    <div class="camera_wrap overflow-hidden relative h-screen" id="camera-slide">
       <div data-src="images/hero-pattern.svg">
         <div class="w-11/12 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div
@@ -86,4 +86,30 @@
     </div>
 
   </section>
+
+  <section class="py-20 lg:py-16">
+    <div class="container grid gap-10 lg:gap-20 lg:grid-cols-2">
+
+      <div class="grid place-items-center">
+        <div>
+          <h2 class="text-slate-900 text-center lg:text-left text-2xl md:text-3xl xl:text-4xl font-bold mb-8">Welcome to A2Z Immigration</h2>
+          <div>
+            <p class="font-nunito font-medium md:text-lg text-slate-600 mb-6">A2Z Immigration Solution Limited has spent the last 5 years as region leader in UK Immigration Law. We have been a fully OISC regulated immigration firm for over 5 years now and have
+              specialised in immigration matters.
+            </p>
+            <p class="font-nunito font-medium md:text-lg text-slate-600">
+              Our expert team have more than 50 years of combined immigration expertise. We have proudly handled more than 30 immigration cases and our general success rate has been over 100% across all application types, so we are the go to experts for all matters regarding UK immigration laws, applications and procedures.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="grid place-items-center">
+        <img src="/images/banner-1.svg" alt="hero banner" class="max-w-md">
+      </div>
+
+    </div>
+  </section>
+
+  
 </main>
